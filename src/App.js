45f1c3d315c830a2683333671df8d4cb5be12fad
yryskul <mail.iriskul@gmail.com>
2022-11-01 16:51:34 +0600

    video1 completed

--- src/App.js
+++ src/App.js
@@ -1,48 +1,87 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  const [isVisibleForm, setFormVisibility] = useState(false);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      let tasks = await fetchTasks();
+      setTasks(tasks);
+    };
+    getTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    return await res.json();
+  };
+
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(task),
+    });
+    let newTask = await res.json();
+    setTasks([...tasks, newTask]);
+  };
 
   const deleteTask = (id) => {
+    fetch("http://localhost:5000/tasks/" + id, { method: "DELETE" });
     setTasks(tasks.filter((t) => t.id != id));
   };
 
   const toggleReminder = (id) => {
+    let task = tasks.find((t) => t.id == id);
+    fetch("http://localhost:5000/tasks/" + id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...task, reminder: !task.reminder }),
+    });
+
     setTasks(
       tasks.map((t) => (t.id == id ? { ...t, reminder: !t.reminder } : t))
     );
   };
 
   return (
-    <div className="container">
-      <Header />
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No Tasks"
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setFormVisibility(!isVisibleForm)}
+          isVisibleForm={isVisibleForm}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {isVisibleForm && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks"
+                )}
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
